87f482e113fdc5325a9b71a70277efc6e995d356
DAROLEND <slabitskyia@gmail.com>
2026-06-08 02:03:02 +0300

    fix: remove non-existent menu_items table from fix script

--- fix_images.php
+++ fix_images.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/db/db.php';
 
-$tables = ['fast_food_items', 'pizza_items', 'dessert_items', 'cold_drink_items', 'coffee_items', 'menu_items'];
+$tables = ['fast_food_items', 'pizza_items', 'dessert_items', 'cold_drink_items', 'coffee_items'];
 $total = 0;
 foreach ($tables as $table) {
     $r = $conn->query("UPDATE `$table` SET image = REPLACE(image, '.png', '.webp') WHERE image LIKE '%.png'");
